6e8247e971a2eed7a32df3e5fab9e9d20ea230e8
Harwas <royharwamasatu06@gmail.com>
2025-07-24 01:17:43 +0700

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,10 @@
-import 'package:catat_meter/firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'firebase_connection.dart';
+import 'screens/home_screen.dart'; // ✅ Panggil HomeScreen dari folder screens
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Inisialisasi Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  // Data dummy pelanggan
-  final Map<String, dynamic> dummyData = {
-    'id': 'P001',
-    'nama': 'Roy Harwa',
-    'qr_code': 'QR123456',
-    'tarif': 1500,
-    'catatan_meter': 'Awal bulan Juli',
-    'alamat': 'Desa Maju Jaya RT 03',
-    'no_hp': '081234567890',
-    'stan_awal': 120,
-    'foto': 'https://example.com/foto.jpg',
-  };
-
-  // Kirim data ke Firebase
-  final dbRef = FirebaseDatabase.instance.ref().child('pelanggan/P001');
-
-  try {
-    await dbRef.set(dummyData);
-    debugPrint('✅ Data dummy berhasil dikirim ke Firebase');
-  } catch (e) {
-    debugPrint('❌ Gagal mengirim data: $e');
-  }
-
+  await FirebaseService.initializeFirebase();
   runApp(const MyApp());
 }
 
@@ -48,26 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Center(
-        child: Text(
-          'Data Dummy telah dikirim ke Firebase!',
-          style: TextStyle(fontSize: 16),
-        ),
-      ),
+      home:  HomeScreen(), // ✅ Panggil dari file screens/home_screen.dart
     );
   }
 }
